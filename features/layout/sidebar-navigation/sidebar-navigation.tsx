f1ca3c47f6ed3fbae96445ab354209a83fe7cf17
CCCFvaldi <fvaldi@gmail.com>
2023-10-24 14:52:28 +0200

    fix navigation

--- features/layout/sidebar-navigation/sidebar-navigation.tsx
+++ features/layout/sidebar-navigation/sidebar-navigation.tsx
@@ -90,7 +90,11 @@ export function SidebarNavigation() {
               iconSrc="/icons/arrow-left.svg"
               isCollapsed={isSidebarCollapsed}
               onClick={() => toggleSidebar()}
-              className={isSidebarCollapsed ? styles.collapseMenuItem : ""}
+              className={
+                isSidebarCollapsed
+                  ? styles.collapseMenuItemCollapsed
+                  : styles.collapseMenuItem
+              }
             />
           </ul>
         </nav>
